d48c8ce14dc18ce8df88d60bf5a71397e8312f65
l3ka <lekic.d7@gmail.com>
2022-12-06 09:56:52 +0100

    Added solution for Day 5 - Tuning Trouble.

---
+++ Day_06/FirstPart.cs
@@ -0,0 +1,44 @@
+﻿namespace Day_06;
+
+public class FirstPart
+{
+    private const int START_OF_PACKET_LENGTH = 4;
+    private const string FILE_NAME = "Day06.txt";
+    private readonly string PATH;
+
+    public FirstPart()
+    {
+        string projectRoot = AppContext.BaseDirectory[..AppContext.BaseDirectory.IndexOf("bin")];
+
+        PATH = Path.Combine(projectRoot, FILE_NAME);
+    }
+
+    public async Task<int> Calculate()
+    {
+        string signal = await File.ReadAllTextAsync(PATH);
+        HashSet<char> characters = new HashSet<char>();
+        int startOfPacketIndex = 0;
+
+        for (int i = 0, j = 0; i < signal.Length; )
+        {
+            if (!characters.Contains(signal[i]))
+            {
+                characters.Add(signal[i]);
+                ++i;
+            }
+            else
+            {
+                characters.Remove(signal[j]);
+                ++j;
+            }
+
+            if (characters.Count == START_OF_PACKET_LENGTH)
+            {
+                startOfPacketIndex = i;
+                break;
+            }
+        }
+
+        return startOfPacketIndex;
+    }
+}
---
+++ Day_06/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Day_06;
+
+/// <summary>
+/// Day 6: Tuning Trouble
+/// </summary>
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine($"Day 06 of the Advent of Code!{Environment.NewLine}");
+
+        await FirstSolution();
+        await SecondSolution();
+    }
+
+    private static async Task FirstSolution()
+    {
+        Console.WriteLine("First Part");
+
+        FirstPart solution = new();
+        int startOfPacketIndex = await solution.Calculate();
+
+        Console.WriteLine($"Start of Packet Marker: {startOfPacketIndex}");
+        Console.WriteLine();
+    }
+
+    private static async Task SecondSolution()
+    {
+        Console.WriteLine("Second Part");
+
+        SecondPart solution = new();
+        int startOfMessageIndex = await solution.Calculate();
+
+        Console.WriteLine($"Start of Message Marker: {startOfMessageIndex}");
+        Console.WriteLine();
+    }
+}
